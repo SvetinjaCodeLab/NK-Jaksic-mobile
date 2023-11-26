667fb4396542be8dc06da2e2a3607c7251c51535
dominikdoric <dominikdoric277@gmail.com>
2023-11-26 12:27:49 +0100

    Implemented news Navigatio drawer

--- composeApp/src/commonMain/kotlin/presentation/screens/news/NewsScreenContent.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/news/NewsScreenContent.kt
@@ -2,17 +2,23 @@ package presentation.screens.news
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -22,43 +28,43 @@ import presentation.composables.drawer.DrawerContent
 @Composable
 fun NewsScreenContent(
     uiState: NewsState,
-    coroutineScope: CoroutineScope
+    coroutineScope: CoroutineScope,
+    navigator: Navigator,
+    drawerState: DrawerState
 ) {
-    val navigator = LocalNavigator.currentOrThrow
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("News Screen") },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                navigationIcon = {
-                    IconButton(onClick = {
-                        coroutineScope.launch {
-                            // scaffoldState.drawerState.open()
-                        }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Default.Menu,
-                            contentDescription = null
-                        )
-                    }
-                }
-            )
-        },
-        /*
+    ModalNavigationDrawer(
+        drawerState = drawerState,
         drawerContent = {
-            DrawerContent(
-                onItemClick = { clickedScreen ->
+            ModalDrawerSheet {
+                DrawerContent { clickedScreen ->
                     navigator.push(clickedScreen)
-                    coroutineScope.launch {
-                        scaffoldState.drawerState.close()
-                    }
                 }
-            )
-        },*/
-        content = {}
-    )
+            }
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("News Screen") },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            coroutineScope.launch {
+                                // scaffoldState.drawerState.open()
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                )
+            },
+            content = {}
+        )
+    }
 }
--- composeApp/src/commonMain/kotlin/presentation/screens/table/TableScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/table/TableScreen.kt
@@ -1,17 +1,25 @@
 package presentation.screens.table
 
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 
 @Composable
 fun TableScreen(viewModel: TableScreenViewModel) {
     val uiState by viewModel.uiState.collectAsState()
     val coroutineScope = rememberCoroutineScope()
+    val navigator = LocalNavigator.currentOrThrow
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
     TableScreenContent(
         uiState = uiState,
-        coroutineScope = coroutineScope
+        coroutineScope = coroutineScope,
+        navigator = navigator,
+        drawerState = drawerState
     )
 }
